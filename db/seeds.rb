7ff3c0a593b1435bba0d984714f681582199058a
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-04-02 05:03:32 +0530

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(greeting_text: 'Hello')
+Greeting.create(greeting_text: 'hallo')
+Greeting.create(greeting_text: 'How you do)')
+Greeting.create(greeting_text: 'Hello,let code')
+Greeting.create(greeting_text: 'Hello me')

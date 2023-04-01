1e46ea9df42c76e883b6ebf9d1121b173aa8007f
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-04-02 04:35:45 +0530

    linters

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,6 @@
 class ApiController < ApplicationController
-    def random_msg
-        @msg = Greeting.find(Greeting.pluck(:id).sample)
-        render json: @msg
-      end
+  def random_msg
+    @msg = Greeting.find(Greeting.pluck(:id).sample)
+    render json: @msg
+  end
 end
